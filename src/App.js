c2fdcd4b8398b3c1337422227886fb8c948ceb5f
eiansd <joh7727@naver,com>
2024-01-05 13:05:26 +0900

    초기세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div></div>;
-}
-
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Router from './shared/Router';
+const queryClient = new QueryClient();
+const App = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    );
+};
 export default App;
