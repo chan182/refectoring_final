96b273eb4c45b1a223def2f20617db2029787655
SuinChoi <ctd2015@gmail.com>
2024-01-17 19:43:07 +0900

    최신소스 반영

--- src/components/mbti_community/CommunityWrite.jsx
+++ src/components/mbti_community/CommunityWrite.jsx
@@ -1,45 +1,50 @@
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
-
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 export default function UpdateTest() {
     // Toast-UI Editor DOM
     const editorRef = useRef();
 
-    // 공지사항과 같은 고정적인 내용을 표시
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
     useEffect(() => {
         // 1. DB에서 가져온 HTML이라고 가정
-        const htmlString = '<h1>h1 태그로 작성한 제목입니다.</h1> <p> p 태그로 작성한 내용입니다.</p>';
+        const htmlString = '';
 
         // 2. Editor DOM 내용에 HTML 주입
         editorRef.current?.getInstance().setHTML(htmlString);
     }, []);
 
     return (
-        // <StBox>
-        //     <StDiv>
-        <>
-            <h1>게시글 작성하기</h1>
-            <Editor
-                ref={editorRef} // useRef로 DOM 연결
-                previewStyle="vertical"
-                height="500px"
-                initialEditType="wysiwyg"
-                toolbarItems={[
-                    ['heading', 'bold', 'italic', 'strike'],
-                    ['hr', 'quote'],
-                    ['ul', 'ol', 'task', 'indent', 'outdent'],
-                    ['table', 'image', 'link'],
-                    ['code', 'codeblock']
-                ]}
-            ></Editor>
-            <button>저장하기</button>
-            <button>글 작성 취소하기</button>
-        </>
-        //     </StDiv>
-        // </StBox>
+        <StBox>
+            <StDiv>
+                <StH1>게시글 작성하기</StH1>
+                <StPeed>
+                    <StInput placeholder="제목을 입력해주세요."></StInput>
+                    <Editor
+                        placeholder="내용을 입력하세요."
+                        ref={editorRef} // useRef로 DOM 연결
+                        previewStyle="vertical"
+                        height="500px"
+                        initialEditType="wysiwyg"
+                        toolbarItems={[
+                            ['heading', 'bold', 'italic', 'strike'],
+                            ['hr', 'quote'],
+                            ['ul', 'ol', 'task', 'indent', 'outdent'],
+                            ['table', 'image', 'link'],
+                            ['code', 'codeblock']
+                        ]}
+                    ></Editor>
+                </StPeed>
+                <StBtns>
+                    <StEditBtn>저장하기</StEditBtn>
+                    <StCancelBtn>글 작성 취소하기</StCancelBtn>
+                </StBtns>
+            </StDiv>
+        </StBox>
     );
 }
 
@@ -54,9 +59,66 @@ const StBox = styled.div`
 
 const StDiv = styled.div`
     width: 60%;
-    height: 80%;
+    height: 90%;
+    display: flex;
+    position: relative;
+    flex-direction: column;
+`;
+
+const StPeed = styled.div`
+    width: 98%;
+    height: 85%;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    margin-left: 1%;
+    border-radius: 10px;
+    background-color: white;
+    border: 1px solid var(--content-border-color);
+`;
+
+const StH1 = styled.h1`
+    font-size: 26px;
+    padding: 10px 0px 10px 20px;
+`;
+
+const StInput = styled.input`
+    width: 100%;
+    height: 47px;
+    border: none;
+    border-radius: 10px;
+    margin-bottom: 12px;
+    padding: 10px 0px 10px 27px;
+    background-color: var(--light-gray);
+`;
+
+const StBtns = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
-    background-color: aliceblue;
+    justify-content: flex-end;
+    width: 99%;
+    height: 100px;
+    margin-top: 10px;
+`;
+
+const StEditBtn = styled.button`
+    width: 180px;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 18px;
+    background-color: var(--main-button-color);
+    color: white;
+`;
+const StCancelBtn = styled.button`
+    width: 180px;
+    height: 40px;
+    margin-left: 10px;
+    border-radius: 5px;
+    font-size: 18px;
+    background-color: var(--light-gray);
+    color: var(--bold-gray);
+    &:hover {
+        background-color: var(--main-button-color);
+        color: white;
+    }
 `;
