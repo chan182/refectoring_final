eda9429bac2f0e86ccc22d4336d4e754ffe7c21d
SuinChoi <ctd2015@gmail.com>
2024-01-19 02:15:00 +0900

    page: MBTI 모임 생성 페이지 와이어프레임 구현 (90% 완성)

--- src/components/mbti_community/CommunityWrite.jsx
+++ src/components/mbti_community/CommunityWrite.jsx
@@ -1,25 +1,13 @@
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
+import PeedEditor from './PeedEditor';
 
 export default function UpdateTest() {
-    // Toast-UI Editor DOM
-    const editorRef = useRef();
-
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
 
     const nav = useNavigate();
-
-    useEffect(() => {
-        // 1. DB에서 가져온 HTML이라고 가정
-        const htmlString = '';
-
-        // 2. Editor DOM 내용에 HTML 주입
-        editorRef.current?.getInstance().setHTML(htmlString);
-    }, []);
 
     return (
         <StBox>
@@ -27,20 +15,7 @@ export default function UpdateTest() {
                 <StH1>게시글 작성하기</StH1>
                 <StPeed>
                     <StInput placeholder="제목을 입력해주세요."></StInput>
-                    <Editor
-                        placeholder="내용을 입력하세요."
-                        ref={editorRef} // useRef로 DOM 연결
-                        previewStyle="vertical"
-                        height="500px"
-                        initialEditType="wysiwyg"
-                        toolbarItems={[
-                            ['heading', 'bold', 'italic', 'strike'],
-                            ['hr', 'quote'],
-                            ['ul', 'ol', 'task', 'indent', 'outdent'],
-                            ['table', 'image', 'link'],
-                            ['code', 'codeblock']
-                        ]}
-                    ></Editor>
+                    <PeedEditor />
                 </StPeed>
                 <StBtns>
                     <StEditBtn>저장하기</StEditBtn>
---
+++ src/components/mbti_community/PeedEditor.jsx
@@ -0,0 +1,35 @@
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import React, { useEffect, useRef } from 'react';
+
+const PeedEditor = () => {
+    // Toast-UI Editor DOM
+    const editorRef = useRef();
+
+    useEffect(() => {
+        // 1. DB에서 가져온 HTML이라고 가정
+        const htmlString = '';
+
+        // 2. Editor DOM 내용에 HTML 주입
+        editorRef.current?.getInstance().setHTML(htmlString);
+    }, []);
+
+    return (
+        <Editor
+            placeholder="내용을 입력하세요."
+            ref={editorRef} // useRef로 DOM 연결
+            previewStyle="vertical"
+            height="500px"
+            initialEditType="wysiwyg"
+            toolbarItems={[
+                ['heading', 'bold', 'italic', 'strike'],
+                ['hr', 'quote'],
+                ['ul', 'ol', 'task', 'indent', 'outdent'],
+                ['table', 'image', 'link'],
+                ['code', 'codeblock']
+            ]}
+        ></Editor>
+    );
+};
+
+export default PeedEditor;
