c53ff6a928d2fbec7aa54137044ef4e1378759ab
gilhun <qkrrlfgns123@gmail.com>
2024-02-05 12:56:12 +0900

    feat: 모임 삭제 버튼 추가 및 검사 페이지 디자인 수정

---
+++ src/components/mbti_matching/textImage.js
@@ -0,0 +1,37 @@
+import ENFJ from '../../assets/mbtiMatching/ENFJ.png';
+import ENFP from '../../assets/mbtiMatching/ENFP.png';
+import ENTJ from '../../assets/mbtiMatching/ENTJ.png';
+import ENTP from '../../assets/mbtiMatching/ENTP.png';
+import ESFJ from '../../assets/mbtiMatching/ESFJ.png';
+import ESFP from '../../assets/mbtiMatching/ESFP.png';
+import ESTJ from '../../assets/mbtiMatching/ESTJ.png';
+import ESTP from '../../assets/mbtiMatching/ESTP.png';
+import INFJ from '../../assets/mbtiMatching/INFJ.png';
+import INFP from '../../assets/mbtiMatching/INFP.png';
+import INTP from '../../assets/mbtiMatching/INTP.png';
+import INTJ from '../../assets/mbtiMatching/INTJ.png';
+import ISTP from '../../assets/mbtiMatching/ISTP.png';
+import ISTJ from '../../assets/mbtiMatching/ISTJ.png';
+import ISFP from '../../assets/mbtiMatching/ISFP.png';
+import ISFJ from '../../assets/mbtiMatching/ISFJ.png';
+import deFaultImage from '../../assets/community/x-square.svg';
+
+export const MBTIimage = {
+    ISTJ: ISTJ,
+    ISFJ: ISFJ,
+    INFJ: INFJ,
+    INTJ: INTJ,
+    ISTP: ISTP,
+    ISFP: ISFP,
+    INFP: INFP,
+    INTP: INTP,
+    ESTP: ESTP,
+    ESFP: ESFP,
+    ENFP: ENFP,
+    ENTP: ENTP,
+    ESTJ: ESTJ,
+    ESFJ: ESFJ,
+    ENFJ: ENFJ,
+    ENTJ: ENTJ,
+    DEFAULT: deFaultImage
+};
